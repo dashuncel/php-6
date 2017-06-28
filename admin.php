94a78854e31bb83c6ebd5c8055a14c8f41d93bd9
dashuncel <korovkinada@ya.ru>
2017-06-28 16:24:50 +0800

    v1.2

--- admin.php
+++ admin.php
@@ -1,15 +1,16 @@
 <?php
   if(isset($_POST['btn'])) {
   	if (isset($_FILES)) {
-  	  $dest="tests";
-  	  $pattern="/^[0-9a-z]*.json$/";
+  	  $tests=[]; // массив тестов
+  	  if (PHP_OS=="WINNT") { $dest="tests\\"; } else {$dest="tests/";}
+  	  $pattern="/^[0-9a-z]*.json$/"; // латинские буквы и цифры.json
   	  if (!file_exists($dest)) { mkdir($dest); }
       foreach($_FILES as $key => $val) {
         // проверка на имя:
         foreach($val['name'] as $sub_key => $filename) {
           preg_match($pattern, $filename, $matches); 
           if (count($matches) < 1) {
-          	echo "Файл $filename неверного формата";
+          	echo "Файл $filename неверного формата<br/>";
           	continue;
           }
           if (move_uploaded_file($val['tmp_name'][$sub_key], $dest.$filename)) {
@@ -38,6 +39,6 @@
     <input type="submit" value="Загрузить тесты" name="btn">
     </fieldset>
   </form>
-  <a href="test.php" target="_blank">Список загруженных тестов</a>
+  <a href="list.php" target="_blank">Список загруженных тестов</a>
 </body>
 </html>
