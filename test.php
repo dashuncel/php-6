2db89ee72e3daf9ea9ef4ce67692eaf00ddd7a29
dashuncel <korovkinada@ya.ru>
2017-06-29 12:31:15 +0800

    v1.6

--- test.php
+++ test.php
@@ -6,12 +6,12 @@ if (isset($_GET['test'])) {
   $result=array_search($_GET['test'].".json", $filelist);
   if ($result >= 0) {
     $data=file_get_contents($filelist[$result]);
-    $data=utf8_encode($data);
     $jsonData=json_decode($data, JSON_UNESCAPED_UNICODE); 
-    print_r($jsonData);
-    echo "<br/>".json_last_error();
+    if (json_last_error() != 0) {
+      echo "Ошибка чтения json файла ".json_last_error(); 
+    }
   } else {
-   echo "Что-то пошло не так. Не могу найти файл с тестом ".$_GET["test"].".json"; 
+    echo "Что-то пошло не так. Не могу найти файл с тестом ".$_GET["test"].".json"; 
   }
 }
 
